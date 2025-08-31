d4339a96a02bee4c15af7ec0aa25463272582e7d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-31 23:57:27 +0900

    landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        A community-driven platform where you can explore, rate, and share the
+        most effective AI prompts. Whether you are writing, coding, designing,
+        or brainstorming, find the perfect prompt to unlock AI's full potential
+      </p>
+    </section>
+  );
 }
